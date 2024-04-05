f92e48157215cfd6e9e565c8459f3a0603733258
trevisancaio <133895557+trevisancaio@users.noreply.github.com>
2024-04-05 11:12:41 -0300

    Create exer04.js

---
+++ 1trimestre/lista_prova/exer04.js
@@ -0,0 +1,6 @@
+let nota1=parseInt(prompt("Digite a nota1"))
+let nota2=parseInt(prompt("Digite a nota2"))
+let nota3=parseInt(prompt("Digite a nota3"))
+let nota4=parseInt(prompt("Digite a nota4"))
+let media=(nota1+nota2+nota3+nota4)/4 
+console.log("A media é ",media)
