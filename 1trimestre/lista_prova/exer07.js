59c3694bb65561355972881f1de16bef9f00eb04
trevisancaio <133895557+trevisancaio@users.noreply.github.com>
2024-04-05 11:20:03 -0300

    Create exer07.js

---
+++ 1trimestre/lista_prova/exer07.js
@@ -0,0 +1,19 @@
+let n1 = parseInt(prompt("Digite um numero"))
+let n2 = parseInt(prompt("Digite um numero"))
+let n3 = parseInt(prompt("Digite um numero"))
+ 
+if (n1>n2){
+     console.log("maior numero",n1)
+}
+     
+else if (n3>n1){
+    print("maior numero",n3)
+}
+    
+else if(n3>n2){
+    print("maior numero",n3)
+}
+    
+else if (n2>n1){
+    print("maior numero",n2)
+}
