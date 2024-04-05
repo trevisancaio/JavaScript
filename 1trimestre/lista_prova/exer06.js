3f664d9daf8f1b16dddfaed4967dc3547fc6dcf2
trevisancaio <133895557+trevisancaio@users.noreply.github.com>
2024-04-05 11:17:07 -0300

    Create exer06.js

---
+++ 1trimestre/lista_prova/exer06.js
@@ -0,0 +1,11 @@
+let a=parseInt(prompt("digite um valor"))
+let b=parseInt(prompt("digite um valor"))
+let c=parseInt(prompt("digite um valor"))
+
+function soma_tres_arg(a, b, c){
+    return a + b + c
+  
+}
+    
+let resultado = soma_tres_arg(a, b, c)
+consloe.log("O resultado é",resultado)
