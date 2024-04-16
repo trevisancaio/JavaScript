8af317dc1d99e632a7bada35595ee378cfabcefe
trevisancaio <133895557+trevisancaio@users.noreply.github.com>
2024-04-16 12:28:34 -0300

    Update trabalho1.js

--- Trabalhos/trabalho1.js
+++ Trabalhos/trabalho1.js
@@ -1,33 +1,30 @@
-function calcularIMC(peso, altura) {
-  return peso / (altura ** 2);
-}
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
 
-while (true) {
-  const altura = parseFloat(prompt("Digite sua altura em metros: "));
-  if (altura <= 0) {
-    break;
-  }
+while True:
+    try:
+        altura = float(input("Digite sua altura em metros: "))
 
-  const peso = parseFloat(prompt("Digite seu peso em quilogramas: "));
-  if (peso <= 0) {
-    break;
-  }
+        if altura <= 0:
+            continue
 
-  const imc = calcularIMC(peso, altura);
-  const imcArredondado = imc.toFixed(2);
-  console.log(`Seu IMC é: ${imcArredondado}`);
+        peso = float(input("Digite seu peso em quilogramas: "))
 
-  if (imcArredondado <= 18.4) {
-    console.log("Você está abaixo do peso.");
-  } else if (imcArredondado <= 24.9) {
-    console.log("Seu peso está normal.");
-  } else if (imcArredondado <= 29.9) {
-    console.log("Você está acima do peso.");
-  } else if (imcArredondado <= 34.9) {
-    console.log("Você está com obesidade grau I.");
-  } else if (imcArredondado <= 39.9) {
-    console.log("Você está com obesidade grau II.");
-  } else {
-    console.log("Você está com obesidade grau III.");
-  }
-}
+        if peso <= 0:
+            continue
+
+        imc = calcular_imc(peso, altura)
+        imc_arredondado = round(imc, 2)
+        print(f"Seu IMC é: {imc_arredondado}")
+
+        if imc_arredondado <= 18.4:
+            print("Você está abaixo do peso.")
+        elif imc_arredondado <= 24.9:
+            print("Seu peso está normal.")
+        elif imc_arredondado <= 29.9:
+            print("Você está acima do peso.")
+        elif imc_arredondado <= 34.9:
+            print("Você está com obesidade grau I.")
+        
+    except ValueError:
+        print("Por favor, insira apenas números.")
